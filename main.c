72566258911b3f546172991b8f1fd2de3e971894
Vito Corl <elhkadrawiaymane@gmail.com>
2025-09-03 17:04:28 +0100

    - Added Search_by_title function to allow users to search for a book by entering its title - Function checks if any books exist before searching and notifies user if stock is empty - Integrated search option into main menu (case 3) for easier access - Displays matching book title and author if found

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 
-int add_book(char title[10][100], char author[10][100], int count) {
-    if (count >= 10) {
+int add_book(char title[100][100], char author[100][100], int count) {
+    if (count >= 100) {
         printf("Book storage is full!\n");
         return count;
     }
@@ -18,7 +19,7 @@ int add_book(char title[10][100], char author[10][100], int count) {
     return count;
 }
 
-void view_books(char title[10][100], char author[10][100], int count) {
+void view_books(char title[100][100], char author[100][100], int count) {
     if (count == 0) {
         printf("No books available.\n");
         return;
@@ -30,11 +31,28 @@ void view_books(char title[10][100], char author[10][100], int count) {
     }
 }
 
+void Search_by_title(char title[100][100], char author[100][100],  int count) {
+    char Search[100];
+    if (count == 0) {
+        printf("No books available.\n");
+        return;
+    }
+    
+    printf("ESearch for a book by its title : ");
+    scanf("%s", Search);
+    for(int i = 0; i < count; i++){
+        int c = strcmp(Search, title[i]);
+        if(c == 0){
+            printf("%d. %s by %s\n", i + 1, title[i], author[i]);
+        }
+    }
+}
+
 
 int main(){
     int choise;
     int count = 0;
-    char title[10][100], Author[10][100];
+    char title[100][100], Author[100][100];
     while(1){
         printf("\n//////////// Menu Book Store ///////////////////\n");
         printf("//////// 1 . Add a book to stock. ////////////////\n");
@@ -48,16 +66,19 @@ int main(){
         
         printf("Enter your choise : ");
         scanf("%d", &choise);
-        for(int i = 0; i < 3; i++){
-            if(choise < 0 || choise > 6)
-                printf("wrong try (0 - 6) : ");
+        if(choise < 0 || choise > 7){
+            printf("\nwrong try (0 - 6) !\n");
         }
+        
         switch (choise) {
             case 1:
                 count = (add_book(title, Author, count));
                 break;
             case 2:
                 view_books(title, Author, count);
+                break;
+            case 3:
+                Search_by_title(title, Author, count);
                 break;
         }
     }
