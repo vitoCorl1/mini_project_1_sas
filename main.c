9dad1d7fb435f9fda317aef64430ccba6405ad64
Vito Corl <elhkadrawiaymane@gmail.com>
2025-09-04 09:29:54 +0100

    add price and quantity

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int add_book(char title[100][100], char author[100][100], int count) {
+int add_book(char title[100][100], char author[100][100], float price[100], int Quantity[100], int count) {
     if (count >= 100) {
         printf("Book storage is full!\n");
         return count;
@@ -13,13 +13,19 @@ int add_book(char title[100][100], char author[100][100], int count) {
     printf("Enter author name : ");
     scanf("%s", author[count]);
 
+    printf("Enter Book price : ");
+    scanf("%f", &price[count]);
+
+    printf("Enter Quantity in stock : ");
+    scanf("%d", &Quantity[count]);
+
     count++;
-    printf("Book added! Now total = %d\n", count);
+    printf("Book added succecfully ! Now total = %d\n", count);
 
     return count;
 }
 
-void view_books(char title[100][100], char author[100][100], int count) {
+void view_books(char title[100][100], char author[100][100], float price[100], int Quantity[100], int count) {
     if (count == 0) {
         printf("No books available.\n");
         return;
@@ -27,11 +33,11 @@ void view_books(char title[100][100], char author[100][100], int count) {
 
     printf("\nAvailable Books:\n");
     for (int i = 0; i < count; i++) {
-        printf("%d. %s by %s\n", i + 1, title[i], author[i]);
+        printf("%d. %s by %s , price of %0.2f$, and the Quantity %d\n", i + 1, title[i], author[i], price[i], Quantity[i]);
     }
 }
 
-void Search_by_title(char title[100][100], char author[100][100],  int count) {
+void Search_by_title(char title[100][100], char author[100][100], float price[100], int Quantity[100], int count) {
     char Search[100];
     if (count == 0) {
         printf("No books available.\n");
@@ -43,16 +49,20 @@ void Search_by_title(char title[100][100], char author[100][100],  int count) {
     for(int i = 0; i < count; i++){
         int c = strcmp(Search, title[i]);
         if(c == 0){
-            printf("%d. %s by %s\n", i + 1, title[i], author[i]);
+            printf("%d. %s by %s , price of %0.2f $, and the Quantity %d\n", i + 1, title[i], author[i], price[i], Quantity[i]);
         }
     }
 }
+
+// void Update_quantity( int Quantity[20][100], int count)
 
 
 int main(){
     int choise;
     int count = 0;
     char title[100][100], Author[100][100];
+    float price[100];
+    int Quantity[100];
     while(1){
         printf("\n//////////// Menu Book Store ///////////////////\n");
         printf("//////// 1 . Add a book to stock. ////////////////\n");
@@ -72,13 +82,15 @@ int main(){
         
         switch (choise) {
             case 1:
-                count = (add_book(title, Author, count));
+                count = (add_book(title, Author, price, Quantity , count));
                 break;
             case 2:
-                view_books(title, Author, count);
+                view_books(title, Author, price, Quantity , count);
                 break;
             case 3:
-                Search_by_title(title, Author, count);
+                Search_by_title(title, Author, price, Quantity , count);
+                break;
+            case 4:
                 break;
         }
     }
